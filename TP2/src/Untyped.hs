c315ebf267ad48fc7b0aa876d7ada09591640991
Jfarizano <juanignaciofarizano@gmail.com>
2020-10-23 11:20:52 -0300

    hecho eval y quote, falta ej 6

--- TP2/src/Untyped.hs
+++ TP2/src/Untyped.hs
@@ -22,30 +22,50 @@ conversion' (App lt1 lt2) xs = let
                                   t1 = conversion' lt1 xs
                                   t2 = conversion' lt2 xs
                                 in (t1 :@: t2)
-conversion' (Abs s lt) xs = Lam (conversion' lt (s:xs))                             
+conversion' (Abs s lt) xs = Lam (conversion' lt (s:xs))                           
 
 -------------------------------
 -- Sección 3
 -------------------------------
 
 vapp :: Value -> Value -> Value
-vapp = undefined
+vapp (VLam f) v2 = f v2
+vapp (VNeutral n) v2 = VNeutral (NApp n v2)
 
 eval :: NameEnv Value -> Term -> Value
 eval e t = eval' t (e, [])
 
 eval' :: Term -> (NameEnv Value, [Value]) -> Value
 eval' (Bound ii) (_, lEnv) = lEnv !! ii
-eval' _          _         = undefined
-
+eval' (Free n) (gEnv, _) = v where Just v = lookup n gEnv
+-- E-AppAbs
+eval' (Lam t1 :@: t2) envs@(gEnv, lEnv) = eval' t1 (gEnv, v:lEnv)
+                                           where v = eval' t2 envs
+-- E-App 1 y E-App2
+eval' (t1 :@: t2) envs = let
+                            v1 = eval' t1 envs
+                            v2 = eval' t2 envs
+                          in vapp v1 v2
+-- Regla E-Abs
+eval' (Lam t) (gEnv, lEnv) = VLam (\x -> eval' t (gEnv, x:lEnv))
 
 --------------------------------
 -- Sección 4 - Mostrando Valores
 --------------------------------
 
 quote :: Value -> Term
-quote = undefined
+quote v = quote' v 0
 
+quote' :: Value -> Int -> Term
+quote' (VNeutral (NFree (Global n))) _ = Free (Global n)
+quote' (VNeutral (NFree (Quote k))) i = Bound (i - k)
+quote' (VNeutral (NApp n v)) i = (quote' (VNeutral n) i) :@: (quote' v i)
+quote' (VLam f) i = (Lam q)
+                     where q = case f (VNeutral (NFree (Quote i))) of
+                                VLam f' -> quote' (VLam f') (i + 1)
+                                VNeutral (NFree (Quote k)) -> Bound (i - k)
+                                VNeutral (NFree (Global n)) -> Free (Global n)
+                                VNeutral (NApp n v) -> (quote' (VNeutral n) i) :@: (quote' v i)
 
 
 
